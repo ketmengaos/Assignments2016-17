cc173cab670005ff54a5d90aeef5a8202354dfb1
Ket-Meng Cheng <ketmengaos>
2016-09-03 01:08:13 -0400

    Final program draft.

--- C++Sort.cpp
+++ C++Sort.cpp
@@ -1,3 +1,15 @@
+//*******************************************************************
+//****|| Ket-Meng Jimmy Cheng - September 6, 2016 - Program #1 ||****
+//****|| ----------------------------------------------------- ||**** 
+//****|| This programs reads from standard input, allocates -- ||****
+//****|| space on using an array, then inserts the numbers --- ||****
+//****|| defined by the user. It then sorts via selection_sort ||****
+//****|| using solely pointers, then asks the user to input a  ||****
+//****|| a value to be found for binary_search. If it exists,  ||****
+//****|| binary_search returns an address--if not, it returns  ||****
+//****|| zero as an integer.                                   ||****
+//*******************************************************************
+
 #include <iostream>
 using namespace std;
 
@@ -16,15 +28,13 @@ void selection_sort(int *low, int *high)
 	}
 	for (int k = 0; k <= count; k++, pnt1++)
 		cout << *pnt1 << endl;
-	
 }
 
-int* binary_search(int *low, int *high, int val)
+int* binary_search(int *low, int *high, int val) 
 {
     while (*high >= *low)
        {
        int *median = ((high-low) / 2 ) + low;
-       cout << "Median: " << *median << " High: " << *high << " Low: : " << *low << endl;
        if (*median == val)
 	       return median;
        else if (val > *median)
@@ -32,7 +42,6 @@ int* binary_search(int *low, int *high, int val)
        else if (val < *median)
 	       high = median - 1;
        }	   
-    cout << "ending reached" << endl;
     return 0;	
 }
 
@@ -53,10 +62,9 @@ int main()
 
 	cout << endl;
 
-	int *p = &numbers[0];
-
 	int size = sizeof(numbers)/sizeof(numbers[0]);
-	selection_sort(&numbers[0], &numbers[size-1]);
-	cout << endl;
-	cout << "Does value exist in memory: (returns 0 if null) " << binary_search(&numbers[0], &numbers[size-1], 2) << endl;
+	selection_sort(&numbers[0], &numbers[size-1]); //Selection_sort method.
+	cout << endl << "Enter an integer to find: ";
+	cin >> amount;
+	cout << "Does value exist in memory (returns 0 if it does not exist): " << binary_search(&numbers[0], &numbers[size-1], amount) << endl; //Tests binary_sort method.
 }
